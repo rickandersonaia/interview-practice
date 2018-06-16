9b573109cd5a78709b27c8c8acacf7b0a7f21cda
Rick Anderson <rick@byobwebsite.com>
2018-06-16 09:34:02 -0700

    Problem 2 solved
    Feat: Completed dice rolling problem

--- app.py
+++ app.py
@@ -10,16 +10,13 @@ import random
 def hello_world():
     return 'Hello World!'
 
-
+# This route creates a random number from 1 to 12 and stores that value in a
+# dice variable.  It then places those numbers in a dictionary and encodes them
 @app.route('/roll')
 def roll_the_dice():
     die1 = random.randint(1, 12)
-    return die1
-
-
-@app.route('/test')
-def hello_test():
-    return 'Hello Test!'
+    die2 = random.randint(1, 12)
+    return json.JSONEncoder().encode({'die1': die1, 'die2': die2})
 
 
 if __name__ == '__main__':
